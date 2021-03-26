bb32322003e66fa9b28170bcacc5e0fbff448945
jorgefelipe <jorgefelipecunha@gmail.com>
2021-03-25 21:39:26 -0300

    :tada: challenge Finalized

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -15,7 +15,7 @@ export function TaskList() {
   const [tasks, setTasks] = useState<Task[]>([]);
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
-  function handleCreateNewTask() {
+  function handleCreateNewTask(){
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
     if(!newTaskTitle) return;
     const newTask = {
